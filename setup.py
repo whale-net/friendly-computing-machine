3eaac09d355c35eac376f9087bbedb943630fd59
qmastermoo <cpsaicontact@gmail.com>
2023-01-28 17:38:19 -0500

    messages printing

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
-setup(name='friendly-computing-machine',
-      version='0.1.0',
-      description='slackbot',
-      #packages=['friendly_computing_machine'],
-      #author='',
-      #author_email='',
-      url='https://github.com/whale-net/friendly-computing-machine',
-      install_requires=['slack_sdk']
-     )
+setup(
+    name="friendly-computing-machine",
+    version="0.1.0",
+    description="slackbot",
+    # packages=['friendly_computing_machine'],
+    # author='',
+    # author_email='',
+    url="https://github.com/whale-net/friendly-computing-machine",
+    install_requires=["slack_sdk", "black"],
+)
