f1b109bdcc64c0a8d6a9037a08cf959bc0d83cf7
qmastermoo <cpsaicontact@gmail.com>
2024-11-30 19:50:06 -0500

    update action for release

--- src/friendly_computing_machine/cli/cli.py
+++ src/friendly_computing_machine/cli/cli.py
@@ -8,11 +8,13 @@ from friendly_computing_machine.db.db import init_engine
 from friendly_computing_machine.bot.cli import app as bot_app
 from friendly_computing_machine.db.cli import migration_app
 from friendly_computing_machine.cli.util import CliContext
+from friendly_computing_machine.cli.tools import app as tool_app
 
 # TODO - multi app - need to check multi app callback logic
 app = typer.Typer()
 app.add_typer(bot_app, name="bot")
 app.add_typer(migration_app, name="migration")
+app.add_typer(tool_app, name="tools")
 
 
 #
---
+++ src/friendly_computing_machine/cli/tools.py
@@ -0,0 +1,27 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def update_helm_chart_version(ref: str, commit_count: int):
+    # crude but it'll do
+
+    if ref.startswith("refs/tags/v"):
+        version = ref[len("refs/tags/v") :]
+    else:
+        version = f"v0.0.{commit_count}"
+
+    file_lines: list[str] = []
+    with open("./charts/friendly-computing-machine/Chart.yaml", "r") as chart:
+        while True:
+            line = chart.readline()
+            if not line:
+                break
+            elif line.startswith("version:"):
+                file_lines.append(f"version: {version}\n")
+            else:
+                file_lines.append(line)
+
+    with open("./charts/workflow-templates/Chart.yaml", "w") as chart:
+        chart.writelines(file_lines)
