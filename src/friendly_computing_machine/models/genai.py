12abccfea0ffc3becc3b5b347882c8637a5baa6f
Alex Harding <alex.harding@whale-net.net>
2025-02-14 19:36:23 -0500

    feat: log gen text requests (#20)
    * feat: more genai functionality
    
    * log

---
+++ src/friendly_computing_machine/models/genai.py
@@ -0,0 +1,40 @@
+import datetime
+
+from sqlmodel import Field
+from sqlalchemy import func, Column, DateTime
+
+from friendly_computing_machine.models.base import Base
+
+
+class GenAITextBase(Base):
+    slack_channel_slack_id: str = Field(index=True)
+    slack_user_slack_id: str = Field(index=True)
+    prompt: str
+    created_at: datetime.datetime = Field(
+        sa_column=Column(
+            DateTime(timezone=True), server_default=func.current_timestamp(), index=True
+        ),
+    )
+    response: str = Field(nullable=True, default=None)
+    response_as_of: datetime.datetime = Field(index=True, nullable=True, default=None)
+
+
+class GenAIText(GenAITextBase, table=True):
+    id: int = Field(default=None, nullable=False, primary_key=True)
+    slack_channel_id: int = Field(
+        nullable=True, foreign_key="slackchannel.id", index=True
+    )
+    slack_user_id: int = Field(nullable=True, foreign_key="slackuser.id", index=True)
+
+
+class GenAITextCreate(GenAITextBase):
+    # TODO surely a better pattern exists
+    def to_genai_text(self) -> GenAIText:
+        return GenAIText(
+            slack_channel_slack_id=self.slack_channel_slack_id,
+            slack_user_slack_id=self.slack_user_slack_id,
+            prompt=self.prompt,
+            created_at=self.created_at,
+            response=self.response,
+            response_as_of=self.response_as_of,
+        )
