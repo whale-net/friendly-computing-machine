25e3b2be14e38082e6015cdb9d8dd5187bdf286f
Christian Mechler <christianmechler1@gmail.com>
2025-04-28 15:41:55 -0600

    .env file check

--- run.sh
+++ run.sh
@@ -3,4 +3,11 @@
 uv venv
 source .venv/bin/activate
 uv sync
-uv run --env-file .env  fcm bot run
+
+if [ -e ".env" ]; then
+    echo ".env file found"
+    uv run --env-file .env fcm bot run
+else
+    echo "no .env file was found"
+    uv run fcm bot run
+fi
